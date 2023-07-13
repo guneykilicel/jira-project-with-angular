8e8e79d4cfc9cba580b0c30acfa59b2383329c91
guneykilicel <kilicelguney@gmail.com>
2023-07-13 11:43:33 +0300

    iskelet oluşturuldu butona basınca dialog açılıyor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from './header/header.component';
+import { BoardsComponent } from './boards/boards.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import { BoardsDialogComponent } from './boards/boards-dialog/boards-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BoardsComponent,
+    BoardsDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/boards/boards-dialog/boards-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boards-dialog',
+  templateUrl: './boards-dialog.component.html',
+  styleUrls: ['./boards-dialog.component.css']
+})
+export class BoardsDialogComponent {
+
+}
